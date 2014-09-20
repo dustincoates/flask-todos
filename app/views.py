f9f2a1d20e68416d0424b627968bc9b23d760d8e
Dustin Coates <dustin@dcoates.com>
2014-09-20 13:58:02 +0000

    Add routes for todo creation

--- app/views.py
+++ app/views.py
@@ -2,4 +2,24 @@ from app import app
 
 @app.route('/')
 def index():
-    return "Hello, World!"
+  return 'Hello, World!'
+
+@app.route('/api/v1/todos', methods = ['GET'])
+def todos():
+  return 'todos index\n'
+
+@app.route('/api/v1/todos/<int:id>', methods = ['GET'])
+def todo(id):
+  return 'Single todo\n'
+
+@app.route('/api/v1/todos', methods = ['POST'])
+def create_todo():
+  return 'You are creating a todo\n'
+
+@app.route('/api/v1/todos/<int:id>', methods = ['PUT'])
+def update_todo(id):
+  return 'You are updating a todo\n'
+
+@app.route('/api/v1/todos/<int:id>', methods = ['DELETE'])
+def delete_todo(id):
+  return 'You are deleting a todo\n'
